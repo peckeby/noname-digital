7f5a3e96c0c40fdc2984a833aa363cd704b0ddfa
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2023-03-03 21:14:38 +0200

    JSON configuration #5

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { App } from 'components/App';
 import './index.css';
 import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
