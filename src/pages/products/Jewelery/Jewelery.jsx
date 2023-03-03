caccbd227132e1e2c0752cb9ee0f08450132e28c
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2023-03-03 21:00:21 +0200

    Change JSON #3

--- src/pages/products/Jewelery/Jewelery.jsx
+++ src/pages/products/Jewelery/Jewelery.jsx
@@ -1,9 +1,3 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import ShopContext from 'context/ShopContext';
 import { Component } from 'react';
 import s from '../ProductStyles.module.scss';
--- src/pages/products/ProductCards.jsx
+++ src/pages/products/ProductCards.jsx
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import s from './ProductStyles.module.scss';
 
 export default function ProductCards({ product }) {
   return (
